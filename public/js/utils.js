871e98d088a5da2fd413fb39cff696bc80575e73
barchinsky <barchinsky@gmail.com>
2015-11-08 16:26:59 +0200

    Added check for goals number while adding the game.

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,1 +1,21 @@
-utils.js
+validateGoals = function(goals,scorers){
+	console.log("utils.validateGoals()");
+
+	console.log("goals:"+goals+" scorers"+scorers[0].goals);
+
+	var goalsSum = 0;
+
+	console.log("scorers:"+scorers.length);
+
+	//if( !scorers.length ) return true; // return true if there is no scorers
+	for(i=0; i < scorers.length; i++){
+		var buf = Number.parseInt(scorers[i].goals);
+
+		goalsSum += isNaN( buf )?0:buf;
+	}
+
+	console.log(goals,goalsSum);
+	return Number.parseInt(goals)===goalsSum;
+
+	console.log("~utils.validateGoals()");
+}
